2eda12fd3387cd3f84428311cca4d2633795abb2
Hiren <hpopat503@rku.ac.in>
2025-03-28 13:55:23 +0530

    chore(#7) Backend Setup Completed using npm init -y

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
+import callRoutes from "./routes/call.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+
+import cors from "cors";
+
+dotenv.config();
+
+
+
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    
+}));
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/calls", callRoutes);
+
+
+server.listen(PORT,()=> {
+    console.log("Server is running on PORT:" + PORT);
+    connectDB();
+});
